2668cba649678aa3d313bf3c8d8995403ee50e8f
David Nichol <david_r_nichol@yahoo.com>
2016-03-24 22:27:12 -0400

    Added fake users repo

--- lib/hasharet/repo.ex
+++ lib/hasharet/repo.ex
@@ -1,3 +1,22 @@
 defmodule Hasharet.Repo do
-  use Ecto.Repo, otp_app: :hasharet
+  # use Ecto.Repo, otp_app: :hasharet
+  @moduledoc """
+    In memory repository
+  """
+
+  def all(Hasharet.User) do
+      [%Hasharet.User{id: "1", name: "Dave", username: "dave", password: "the cat ran down the road!"}]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+      Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+      Enum.find all(module), fn map -> 
+        Enum.all?(params, fn {key, val } -> Map.get(map, key) == val end)
+      end
+  end
 end
